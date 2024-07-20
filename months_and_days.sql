ffbd676e4f5013e65decc5e86d0410d6a0f151eb
Nos-app <82891135+Nos-app@users.noreply.github.com>
2024-07-20 23:05:23 +0300

    Fix sql, reqursive dont work

--- months_and_days.sql
+++ months_and_days.sql
@@ -1,4 +1,4 @@
-WITH RECURSIVE Months AS (
+WITH Months AS (
     SELECT 1 AS month_number
     UNION ALL
     SELECT month_number + 1
@@ -8,4 +8,5 @@ WITH RECURSIVE Months AS (
 SELECT 
     month_number AS Month,
     DAY(EOMONTH(DATEFROMPARTS(YEAR(GETDATE()), month_number, 1))) AS Days
-FROM Months;
+FROM Months
+OPTION (MAXRECURSION 12);
