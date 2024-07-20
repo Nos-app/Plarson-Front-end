bfdc64c62eb5f6e604b0175fbb7fd9f6d78e3f9d
Nos-app <82891135+Nos-app@users.noreply.github.com>
2024-07-20 17:32:45 +0300

    Initial commit

---
+++ months_and_days.sql
@@ -0,0 +1,11 @@
+WITH RECURSIVE Months AS (
+    SELECT 1 AS month_number
+    UNION ALL
+    SELECT month_number + 1
+    FROM Months
+    WHERE month_number < 12
+)
+SELECT 
+    month_number AS Month,
+    DAY(EOMONTH(DATEFROMPARTS(YEAR(GETDATE()), month_number, 1))) AS Days
+FROM Months;
---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+    alert('Страница полностью загружена');
+    console.log('Страница полностью загружена');
+});
